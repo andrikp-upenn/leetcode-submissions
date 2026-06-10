f17c5f8c39b7e6aabb2c58d503b5085387f8f88b
Andrik Puentes <179980311+andrikp-upenn@users.noreply.github.com>
2026-06-10 13:34:01 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,50 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        """
+        Given a string s, return true if it is a palindrome, otherwise return false.
+
+        A palindrome is a string that reads the same forward and backward. 
+        
+        It is also case-insensitive and ignores all non-alphanumeric characters.
+
+        Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
+        """
+        # Two pointer solution O(1)
+        # Check the start and end of the string.
+        # and move toward eachother
+
+        start = 0
+        end = len(s) - 1
+
+        while start < end:
+            # check for alphanumerical chars
+
+            # Check if they're not alnum
+            # if False --> move both
+            if s[start].isalnum() == False:
+                start += 1
+            elif s[end].isalnum() == False:
+                end -= 1
+            
+            # If True --> check and continue
+            elif s[start].lower() == s[end].lower():
+                start += 1
+                end -= 1
+            else:
+                return False
+
+            # If exit loop successfully:
+        return True
+
+# 31/31 test cases
+# Memory 7.7 MB
+# Time: 38ms
+# Beats 99.59%
+                    
+
+
+                        
+                
+                    
+            
+
